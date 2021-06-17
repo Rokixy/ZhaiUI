8352556b2f058fb96dfc45ee33ab14f09e681d3d
rokixy <zzubillzzu@126.com>
2021-06-17 20:08:53 +0800

    使用 chai.expect 添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -15,3 +15,86 @@ new Vue({
         loading3: false,
     }
 })
+
+// 单元测试
+import chai from 'chai'
+const expect = chai.expect
+
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-setting')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting',
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'right'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting',
+        }
+    })
+    button.$mount()
+    button.$on('click', function () {
+        expect(1).to.eq(1)
+    })
+    let buttonEl = button.$el
+    buttonEl.click()
+    button.$el.remove()
+    button.$destroy()
+}
