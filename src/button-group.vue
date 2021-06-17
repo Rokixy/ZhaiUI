4b623ae6ab5bbfe64f8e7f9f6682a9267cce2df7
rokixy <zzubillzzu@126.com>
2021-06-17 17:31:11 +0800

    第一个元素不需要 margin-left: -1px

--- src/button-group.vue
+++ src/button-group.vue
@@ -26,7 +26,9 @@ export default defineComponent({
   vertical-align: middle;
   > .zh-button {
     border-radius: 0;
-    margin-left: -1px;
+    &:not(:first-child) {
+      margin-left: -1px;
+    }
     &:first-child {
       border-top-left-radius: var(--border-radius);
       border-bottom-left-radius: var(--border-radius);
