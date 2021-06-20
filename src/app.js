acc14b463605fc68192b5b358dd7e3cd6f012000
rokixy <zzubillzzu@126.com>
2021-06-20 15:31:29 +0800

    初步完成 row 和 col

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import Button from './button'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('zh-button', Button)
 Vue.component('zh-button-group', ButtonGroup)
 Vue.component('zh-input', Input)
+Vue.component('zh-row', Row)
+Vue.component('zh-col', Col)
 
 new Vue({
     el: '#app',
---
+++ src/col.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+  props: {
+    span: [Number, String],
+  },
+});
+</script>
+
+
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  background: grey;
+  width: 50%;
+  border: 1px solid red;
+
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n/24) * 100%;
+    }
+  }
+}
+</style>
