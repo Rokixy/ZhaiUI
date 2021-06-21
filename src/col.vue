0092eb52a6dbf0a1bfb37f7604a71bca1b42ceb8
rokixy <zzubillzzu@126.com>
2021-06-21 14:11:17 +0800

    基本完成 span 和 gutter

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div
+    class="col"
+    :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+    :style="{ paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px' }"
+  >
+    <div style="border: 1px solid green">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -11,6 +17,12 @@ export default defineComponent({
   setup() {},
   props: {
     span: [Number, String],
+    offset: [Number, String],
+  },
+  data() {
+    return {
+      gutter: 0,
+    };
   },
 });
 </script>
@@ -19,14 +31,17 @@ export default defineComponent({
 <style lang="scss" scoped>
 .col {
   height: 100px;
-  background: grey;
   width: 50%;
-  border: 1px solid red;
-
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,29 @@
 <template>
-  <div class="row">
+  <div
+    class="row"
+    :style="{ marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px' }"
+  >
     <slot></slot>
   </div>
 </template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+  props: {
+    gutter: {
+      type: [Number, String],
+    },
+  },
+  mounted() {
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter;
+    });
+  },
+});
+</script>
 
 <style lang="scss" scoped>
 .row {
