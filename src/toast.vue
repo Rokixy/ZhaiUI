089a1bdf75b0dc6a84b839b688199c8e6c611821
rokixy <zzubillzzu@126.com>
2021-06-28 21:37:33 +0800

    实现 autoClose closeButton enableHtml 多行文字 等功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="toast">
-    <slot></slot>
+  <div class="toast" ref="wrapper">
+    <div class="message">
+      <slot v-if="!enabelHtml"></slot>
+      <div v-else v-html="$slots.default[0]"></div>
+    </div>
+    <div class="line" ref="line"></div>
+    <span class="close" v-if="closeButton" @click="onClickClose">
+      {{ closeButton.text }}
+    </span>
   </div>
 </template>
 
@@ -9,18 +16,71 @@ import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
   setup() {},
+  props: {
+    autoClose: {
+      type: Boolean,
+      default: true,
+    },
+    autoCloseDelay: {
+      type: Number,
+      default: 3,
+    },
+    closeButton: {
+      type: Object,
+      default: () => {
+        return {
+          text: "关闭",
+          callback: undefined,
+        };
+      },
+    },
+    enabelHtml: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  mounted() {
+    this.updateStyle();
+    this.closeAfterDelay();
+  },
+  methods: {
+    updateStyle() {
+      this.$nextTick(() => {
+        this.$refs.line.style.height = `${
+          this.$refs.wrapper.getBoundingClientRect().height
+        }px`;
+      });
+    },
+    closeAfterDelay() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.close();
+        }, this.autoCloseDelay * 1000);
+      }
+    },
+    close() {
+      this.$el.remove();
+      this.$destroy();
+    },
+    onClickClose() {
+      this.close();
+      if (this.closeButton && typeof this.closeButton.callback === "function") {
+        this.closeButton.callback();
+      }
+    },
+  },
 });
 </script>
 
 
 <style lang="scss" scoped>
 $font-size: 14px;
-$toast-height: 40px;
+$toast-min-height: 40px;
 $toast-bg: rgba(0, 0, 0, 0.75);
 .toast {
   font-size: $font-size;
   line-height: 1.8;
-  height: $toast-height;
+  min-height: $toast-min-height;
   position: fixed;
   top: 0;
   left: 50%;
@@ -32,5 +92,17 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
   color: white;
   padding: 0 16px;
+}
+.close {
+  padding-left: 16px;
+  flex-shrink: 0;
+}
+.line {
+  height: 100%;
+  border-left: 1px solid #888;
+  margin-left: 16px;
+}
+.message {
+  padding: 8px 0;
 }
 </style>
