8f1f125a810888bfcdcf291c692ceb0d6ab3fa6c
rokixy <zzubillzzu@126.com>
2021-06-29 14:33:15 +0800

    修复自动测试时，关闭按钮点击过快导致报错的问题

--- test/toast.test.js
+++ test/toast.test.js
@@ -28,7 +28,7 @@ describe('Toast', () => {
             })
         })
 
-        it('接受 closeButton', () => {
+        it('接受 closeButton', (done) => {
             const callback = sinon.fake()
             const Constructor = Vue.extend(Toast)
             const vm = new Constructor({
@@ -41,8 +41,11 @@ describe('Toast', () => {
             }).$mount()
             let closeButton = vm.$el.querySelector('.close')
             expect(closeButton.textContent.trim()).to.eq('text')
-            closeButton.click()     // 自动测试通过，但 chrome headless 报错
-            expect(callback).to.have.been.called
+            setTimeout(() => {
+                closeButton.click()
+                expect(callback).to.have.been.called
+                done()
+            }, 200)
         })
 
         it('接受 HTML', () => {
