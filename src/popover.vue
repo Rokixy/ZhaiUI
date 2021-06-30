f45f195ae5ff4badd4eac330b2dad109dffc06b3
rokixy <zzubillzzu@126.com>
2021-06-30 22:26:13 +0800

    popover 功能基本完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,11 +1,6 @@
 <template>
-  <div class="popover" @click.stop="aaa">
-    <div
-      ref="contentWrapper"
-      class="content-wrapper"
-      v-if="visible"
-      @click.stop
-    >
+  <div class="popover" @click.stop="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -23,20 +18,39 @@ export default defineComponent({
     return { visible: false };
   },
   methods: {
-    aaa() {
-      this.visible = !this.visible;
-      if (this.visible === true) {
-        this.$nextTick(() => {
-          document.body.appendChild(this.$refs.contentWrapper);
-          let { top, left } = this.$refs.triggerWrapper.getBoundingClientRect();
-          this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
-          this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
-          let eventHandl = () => {
-            this.visible = false;
-            document.removeEventListener("click", eventHandl);
-          };
-          document.addEventListener("click", eventHandl);
-        });
+    locateContent() {
+      document.body.appendChild(this.$refs.contentWrapper);
+      let { top, left } = this.$refs.triggerWrapper.getBoundingClientRect();
+      this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
+      this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
+    },
+    onClickDocument(e) {
+      if (
+        this.$refs.contentWrapper.contains(e.target) ||
+        this.$refs.popover.contains(e.target)
+      ) {
+        return;
+      }
+      this.close();
+    },
+    open() {
+      this.visible = true;
+      this.$nextTick(() => {
+        this.locateContent();
+        document.addEventListener("click", this.onClickDocument);
+      });
+    },
+    close() {
+      this.visible = false;
+      document.removeEventListener("click", this.onClickDocument);
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        if (this.visible) {
+          this.close();
+        } else {
+          this.open();
+        }
       }
     },
   },
@@ -51,7 +65,6 @@ export default defineComponent({
 }
 .content-wrapper {
   position: absolute;
-  bottom: 100%;
   left: 0;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
   transform: translateY(-100%);
