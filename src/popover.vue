f128601c2c4ade6dc8e45b208ce2635f3cee20c3
rokixy <zzubillzzu@126.com>
2021-06-30 20:34:37 +0800

    实现点击显示/隐藏

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click="aaa">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="aaa">
+    <div class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
     <slot></slot>
@@ -18,6 +18,15 @@ export default defineComponent({
   methods: {
     aaa() {
       this.visible = !this.visible;
+      if (this.visible === true) {
+        this.$nextTick(() => {
+          let eventHandl = () => {
+            this.visible = false;
+            document.removeEventListener("click", eventHandl);
+          };
+          document.addEventListener("click", eventHandl);
+        });
+      }
     },
   },
 });
