6cd82bb55598cddb7623e03c02e7b75c523162cc
rokixy <zzubillzzu@126.com>
2021-07-04 00:08:18 +0800

    创建官网

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <zh-button>默认按钮</zh-button>
+    <zh-button icon="setting">默认按钮</zh-button>
+    <zh-button :loading="true">默认按钮</zh-button>
+    <zh-button disabled>默认按钮</zh-button>
+
+    <pre><code>{{code}}</code></pre>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+import Button from "../../../src/button.vue";
+
+export default defineComponent({
+  setup() {},
+  components: {
+    "zh-button": Button,
+  },
+  data() {
+    return {
+      code: `
+            <zh-button>默认按钮</zh-button>
+            <zh-button icon="settings">默认按钮</zh-button>
+            <zh-button :loading="true">默认按钮</zh-button>
+            <zh-button disabled>默认按钮</zh-button>
+        `,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    title: '宅 UI',
+    description: '一个基于 Vue2 的 UI 框架',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: [
+            {
+                title: '入门',
+                children: [
+                    ['/install/', '安装'],
+                    ['/get-started/', '快速上手'],
+                ]
+            },
+            {
+                title: '组件',
+                children: [
+                    ['/components/button', '按钮']
+                ]
+            }
+        ]
+    }
+}
